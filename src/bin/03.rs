b7005070c8995177f4dadf0ee63e0804727754c2
eb <eb@eb2.co>
2022-12-03 01:32:43 -0700

    Day 03 part 1 solution

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,9 +1,38 @@
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let sum_item_priorities = input.lines().map(|s| search_rucksack(s).unwrap()).sum();
+
+    Some(sum_item_priorities)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn find_priority(_item: char) -> Option<u32> {
+    if _item as u32 >= 97 {
+        Some(_item as u32 - 97 + 1)
+    } else {
+        Some(_item as u32 - 65 + 27)
+    }
+}
+
+fn search_rucksack(rucksack: &str) -> Option<u32> {
+    let mut priority = 0;
+    let c2 = rucksack
+        .chars()
+        .rev()
+        .take(rucksack.len() / 2)
+        .collect_vec();
+
+    for c in rucksack.chars().take(rucksack.len() / 2) {
+        if c2.contains(&c) {
+            priority = find_priority(c).unwrap();
+        }
+    }
+
+    Some(priority)
 }
 
 fn main() {
@@ -19,7 +48,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(157));
     }
 
     #[test]
