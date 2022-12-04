54e967bc06a69f4ece20008ed10348cf747474a7
eb <eb@eb2.co>
2022-12-04 08:57:59 -0700

    cleanup part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,21 +1,12 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    input.lines().map(play_match_rules1).sum::<Option<u32>>()
+    input.lines().map(outcomes_part1).sum::<Option<u32>>()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    input.lines().map(play_match_rules2).sum::<Option<u32>>()
+    input.lines().map(outcomes_part2).sum::<Option<u32>>()
 }
 
-fn score(throw: char) -> Option<u32> {
-    match throw {
-        'A' | 'X' => Some(1),
-        'B' | 'Y' => Some(2),
-        'C' | 'Z' => Some(3),
-        _ => None,
-    }
-}
-
-fn play_match_rules1(game: &str) -> Option<u32> {
+fn outcomes_part1(game: &str) -> Option<u32> {
     match game {
         "A X" => Some(4),
         "A Y" => Some(8),
@@ -30,23 +21,18 @@ fn play_match_rules1(game: &str) -> Option<u32> {
     }
 }
 
-fn play_match_rules2(game: &str) -> Option<u32> {
-    let opponent = game.chars().nth(0).unwrap();
-    let player = game.chars().nth(2).unwrap();
-    let opponent_score = score(opponent);
-
-    if player == 'Y' {
-        Some(3 + opponent_score.unwrap())
-    } else if player == 'X' {
-        if opponent_score == Some(1) {
-            Some(3)
-        } else {
-            Some(opponent_score.unwrap() - 1)
-        }
-    } else if opponent_score == Some(3) {
-        Some(7)
-    } else {
-        Some(6 + (opponent_score.unwrap() + 1))
+fn outcomes_part2(game: &str) -> Option<u32> {
+    match game {
+        "A X" => Some(3),
+        "A Y" => Some(4),
+        "A Z" => Some(8),
+        "B X" => Some(1),
+        "B Y" => Some(5),
+        "B Z" => Some(9),
+        "C X" => Some(2),
+        "C Y" => Some(6),
+        "C Z" => Some(7),
+        _ => None,
     }
 }
 
