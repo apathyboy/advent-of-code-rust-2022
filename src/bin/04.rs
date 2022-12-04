32d80f48865c6a1457059af89b4db2058e976513
eb <eb@eb2.co>
2022-12-04 10:28:47 -0700

    Day 04 solutions

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,9 +1,27 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(input.lines().map(parse).filter(contains).count())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    Some(input.lines().map(parse).filter(overlaps).count())
+}
+
+fn contains(input: &Vec<u32>) -> bool {
+    (input[0] <= input[2] && input[1] >= input[3]) || (input[0] >= input[2] && input[1] <= input[3])
+}
+
+fn overlaps(input: &Vec<u32>) -> bool {
+    (input[0] >= input[2] && input[0] <= input[3])
+        || (input[1] >= input[2] && input[1] <= input[3])
+        || (input[2] >= input[0] && input[2] <= input[1])
+        || (input[3] >= input[0] && input[3] <= input[1])
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    input
+        .split(",")
+        .flat_map(|s| s.split("-").map(|v| v.parse().unwrap()))
+        .collect()
 }
 
 fn main() {
@@ -19,12 +37,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
