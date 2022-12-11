7cf269555b6cfd34373bcf67a7f4eb3b43e8762a
eb <eb@eb2.co>
2022-12-11 02:40:20 -0700

    Day 11 part 2 solution

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -34,31 +34,70 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(monkey_business_level)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut monkeys = parse(input);
+
+    let lcm = monkeys
+        .iter()
+        .map(|m| m.test_val)
+        .reduce(|accum, item| accum * item)
+        .unwrap();
+
+    for _ in 0..10000 {
+        for i in 0..monkeys.len() {
+            monkeys[i].items.sort_by(|a, b| b.cmp(a));
+
+            while let Some(mut item) = monkeys[i].items.pop() {
+                monkeys[i].inspected_count += 1;
+                item = perform_operation(&(monkeys[i].operation), item);
+
+                if item > lcm {
+                    item %= lcm;
+                }
+
+                if item % monkeys[i].test_val == 0 {
+                    let true_target = monkeys[i].true_target;
+                    monkeys[true_target].items.push(item);
+                } else {
+                    let false_target = monkeys[i].false_target;
+                    monkeys[false_target].items.push(item);
+                }
+            }
+        }
+    }
+
+    let monkey_business_level = monkeys
+        .iter()
+        .map(|m| m.inspected_count as u64)
+        .sorted_by(|a, b| Ord::cmp(&b, &a))
+        .take(2)
+        .reduce(|accum, item| accum * item)
+        .unwrap() as u64;
+
+    Some(monkey_business_level)
 }
 
 struct Monkey {
-    items: Vec<u32>,
+    items: Vec<u64>,
     true_target: usize,
     false_target: usize,
-    test_val: u32,
+    test_val: u64,
     operation: String,
     inspected_count: usize,
 }
 
-fn perform_operation(operation: &str, old_val: u32) -> u32 {
+fn perform_operation(operation: &str, old_val: u64) -> u64 {
     let (first, rest) = operation.split_once(' ').unwrap();
     let (op, second) = rest.split_once(' ').unwrap();
 
     let left_val = match first {
         "old" => old_val,
-        _ => first.parse::<u32>().unwrap(),
+        _ => first.parse::<u64>().unwrap(),
     };
 
     let right_val = match second {
         "old" => old_val,
-        _ => second.parse::<u32>().unwrap(),
+        _ => second.parse::<u64>().unwrap(),
     };
 
     if op == "+" {
@@ -76,12 +115,12 @@ fn parse(input: &str) -> Vec<Monkey> {
 
             let items = lines[1][18..]
                 .split(", ")
-                .map(|s| s.parse::<u32>().unwrap())
+                .map(|s| s.parse::<u64>().unwrap())
                 .sorted()
                 .collect_vec();
             let true_target = lines[4][29..].parse::<usize>().unwrap();
             let false_target = lines[5][30..].parse::<usize>().unwrap();
-            let test_val = lines[3][21..].parse::<u32>().unwrap();
+            let test_val = lines[3][21..].parse::<u64>().unwrap();
             let operation = lines[2][19..].to_string();
 
             Monkey {
@@ -115,6 +154,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 11);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(2713310158));
     }
 }
