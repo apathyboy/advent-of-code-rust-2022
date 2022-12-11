1cd74a3d8dbdaf553e56edb82be826f1c92423fe
eb <eb@eb2.co>
2022-12-11 00:53:10 -0700

    Day 11 part 1 solution

---
+++ src/bin/11.rs
@@ -0,0 +1,120 @@
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut monkeys = parse(input);
+
+    for _ in 0..20 {
+        for i in 0..monkeys.len() {
+            monkeys[i].items.sort_by(|a, b| b.cmp(a));
+
+            while let Some(mut item) = monkeys[i].items.pop() {
+                monkeys[i].inspected_count += 1;
+                item = perform_operation(&(monkeys[i].operation), item);
+                item /= 3;
+
+                if item % monkeys[i].test_val == 0 {
+                    let true_target = monkeys[i].true_target;
+                    monkeys[true_target].items.push(item);
+                } else {
+                    let false_target = monkeys[i].false_target;
+                    monkeys[false_target].items.push(item);
+                }
+            }
+        }
+    }
+
+    let monkey_business_level = monkeys
+        .iter()
+        .map(|m| m.inspected_count)
+        .sorted_by(|a, b| Ord::cmp(&b, &a))
+        .take(2)
+        .reduce(|accum, item| accum * item)
+        .unwrap() as u32;
+
+    Some(monkey_business_level)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+struct Monkey {
+    items: Vec<u32>,
+    true_target: usize,
+    false_target: usize,
+    test_val: u32,
+    operation: String,
+    inspected_count: usize,
+}
+
+fn perform_operation(operation: &str, old_val: u32) -> u32 {
+    let (first, rest) = operation.split_once(' ').unwrap();
+    let (op, second) = rest.split_once(' ').unwrap();
+
+    let left_val = match first {
+        "old" => old_val,
+        _ => first.parse::<u32>().unwrap(),
+    };
+
+    let right_val = match second {
+        "old" => old_val,
+        _ => second.parse::<u32>().unwrap(),
+    };
+
+    if op == "+" {
+        left_val + right_val
+    } else {
+        left_val * right_val
+    }
+}
+
+fn parse(input: &str) -> Vec<Monkey> {
+    input
+        .split("\n\n")
+        .map(|s| -> Monkey {
+            let lines = s.lines().collect_vec();
+
+            let items = lines[1][18..]
+                .split(", ")
+                .map(|s| s.parse::<u32>().unwrap())
+                .sorted()
+                .collect_vec();
+            let true_target = lines[4][29..].parse::<usize>().unwrap();
+            let false_target = lines[5][30..].parse::<usize>().unwrap();
+            let test_val = lines[3][21..].parse::<u32>().unwrap();
+            let operation = lines[2][19..].to_string();
+
+            Monkey {
+                items,
+                true_target,
+                false_target,
+                test_val,
+                operation,
+                inspected_count: 0,
+            }
+        })
+        .collect_vec()
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 11);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 11);
+        assert_eq!(part_one(&input), Some(10605));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 11);
+        assert_eq!(part_two(&input), None);
+    }
+}
