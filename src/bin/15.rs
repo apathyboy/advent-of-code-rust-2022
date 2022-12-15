bf9a234874c82540263aa391e4d95e0c0cc8b438
Eric Barr <eb@eb2.co>
2022-12-15 03:36:49 -0700

    Day 15 part 1 solution

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,10 +1,38 @@
 use itertools::Itertools;
 use regex::Regex;
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     let _sensor_beacon_pairs = parse(input);
+    let beacons = _sensor_beacon_pairs.iter().map(|p| p.1).collect_vec();
 
-    None
+    let mut non_beacon_locations = Vec::new();
+    let n = 2000000;
+
+    for (sensor, beacon) in _sensor_beacon_pairs {
+        let manhattan = (sensor.0 - beacon.0).abs() + (sensor.1 - beacon.1).abs();
+        let y_diff = (sensor.1 - n).abs();
+
+        if y_diff > manhattan {
+            continue;
+        }
+
+        let x_remainder = manhattan - y_diff;
+
+        for x in sensor.0 - x_remainder..=sensor.0 + x_remainder {
+            if !beacons.contains(&(x, n)) {
+                non_beacon_locations.push((x, n));
+            }
+        }
+    }
+
+    Some(non_beacon_locations.iter().unique().count())
+
+    // loop over all pairs
+    // get manhattan from sensor to beacon
+    // find y distance from sensor to y=n (10 for example 2000000 for puzzle)
+    // diff y dist and manhattan distance <-> are the x positions intersected by sensor coverage and y of interest
+    // cache all non-beacon/sensor points of intersection
+    // count of all points of intersection
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -51,7 +79,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 15);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(26));
     }
 
     #[test]
